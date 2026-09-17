97d01b1aaf6d8d732aa4fef97da7232f496d578b
9022392 <9022392@student.zadkine.nl>
2026-09-17 10:23:35 +0200

    Added units
    -Added a unit that produces cream automatically
    -Added store to buy units

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ count.innerText = 0;
 function countUpdate(){count.innerText = cream;};
 
 click.addEventListener("click", function() {
-    cream = cream + 1;
+    cream += 1;
     countUpdate()
 })
+
+//units
+
+let cursor = 0;
+let cursorPrice = 15;
+let cursorRate = 1;
+
+const cursorBuy = document.getElementById("cursorStore");
+const cursorCount = document.getElementById("cursorUnits");
+
+cursorCount.innerText = 0;
+function cursorUpdate(){cursorCount.innerText = cursor;};
+
+cursorBuy.addEventListener("click", function() {
+    if (cream >= cursorPrice) {
+        cream -= cursorPrice;
+        cursor += 1;
+        countUpdate();
+        cursorUpdate();
+    };
+});
+
+setInterval(() => {
+    cream += cursor*cursorRate;
+    countUpdate();
+}, 1000);
