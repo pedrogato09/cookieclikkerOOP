345dcc76795003bfc425bde91d78267cd64404fc
9022392 <9022392@student.zadkine.nl>
2026-09-22 10:48:31 +0200

    OOP
    -Production units are now implemented via Classes and Objects
    -Price of units now goes up after a purchase
    -Tweaked HTML file

--- script.js
+++ script.js
@@ -13,117 +13,54 @@ click.addEventListener("click", function() {
 
 //units
 
-/*
-let cursor = 0;
-let cursorPrice = 15;
-let cursorRate = 1;
-
-const cursorBuy = document.getElementById("cursorStore");
-const cursorCount = document.getElementById("cursorUnits");
-
-cursorCount.innerText = 0;
-function cursorUpdate(){cursorCount.innerText = cursor;};
-
-cursorBuy.addEventListener("click", function() {
-    if (cream >= cursorPrice) {
-        cream -= cursorPrice;
-        cursor += 1;
-        countUpdate();
-        cursorUpdate();
-    };
-});
-
-setInterval(() => {
-    cream += cursor*cursorRate;
-    countUpdate();
-}, 1000);
-*/
-
 class Unit {
     constructor(name, price, rate){
         this.name = name;
         this.count = 0;
-        this.price = Math.round(price*1.1**this.count);
+        this.basePrice = price;
+        this.price = price;
         this.rate = rate;
+
         this.units = document.getElementById(this.name + "Units");
         this.store = document.getElementById(this.name + "Store");
+        this.cost = document.getElementById(this.name + "Cost");
+
+        this.store.addEventListener("click", () => {
+            this.buy();
+        });
+        this.unitUpdate()
+        
+    }
+
+    unitUpdate(){
+        this.units.innerText = this.count;
+        this.cost.innerText = this.price;
+    }
+
+    buy(){
+        if (cream >= this.price) {
+            cream -= this.price;
+            this.price = Math.round(this.basePrice*1.2**(this.count + 1));
+            this.count += 1;
+            countUpdate();
+            this.unitUpdate();
+        }
+    }
+
+    prod(){
+        return Math.round(this.count*this.rate);
     }
 }
 
-const test = new Unit("cursor", 15, 1);
+const cursor = new Unit("cursor", 15, 1);
+const grandma = new Unit("grandma", 100, 5);
+const mine = new Unit("mine", 500, 20);
+const factory = new Unit("factory", 3000, 20);
 
-let grandma = 0;
-let grandmaPrice = 100;
-let grandmaRate = 5;
 
-const grandmaBuy = document.getElementById("grandmaStore");
-const grandmaCount = document.getElementById("grandmaUnits");
-
-grandmaCount.innerText = 0;
-function grandmaUpdate(){grandmaCount.innerText = grandma;};
-
-grandmaBuy.addEventListener("click", function() {
-    if (cream >= grandmaPrice) {
-        cream -= grandmaPrice;
-        grandma += 1;
-        countUpdate();
-        grandmaUpdate();
-    };
-});
 
 setInterval(() => {
-    cream += grandma*grandmaRate;
+    cream += cursor.prod() + grandma.prod() + mine.prod() + factory.prod();
     countUpdate();
 }, 1000);
-
-let mine = 0;
-let minePrice = 500;
-let mineRate = 20;
-
-const mineBuy = document.getElementById("mineStore");
-const mineCount = document.getElementById("mineUnits");
-
-mineCount.innerText = 0;
-function mineUpdate(){mineCount.innerText = mine;};
-
-mineBuy.addEventListener("click", function() {
-    if (cream >= minePrice) {
-        cream -= minePrice;
-        mine += 1;
-        countUpdate();
-        mineUpdate();
-    };
-});
-
-setInterval(() => {
-    cream += mine*mineRate;
-    countUpdate();
-}, 1000);
-
-let factory = 0;
-let factoryPrice = 3000;
-let factoryRate = 100;
-
-const factoryBuy = document.getElementById("factoryStore");
-const factoryCount = document.getElementById("factoryUnits");
-
-factoryCount.innerText = 0;
-function factoryUpdate(){factoryCount.innerText = factory;};
-
-factoryBuy.addEventListener("click", function() {
-    if (cream >= factoryPrice) {
-        cream -= factoryPrice;
-        factory += 1;
-        countUpdate();
-        factoryUpdate();
-    };
-});
-
-setInterval(() => {
-    cream += factory*factoryRate;
-    countUpdate();
-}, 1000);
-
-
-
 
